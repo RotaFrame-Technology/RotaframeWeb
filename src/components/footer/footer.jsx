a7493915fc5f0a5d60dd565b4d6c9a9017f140df
Hiran Kavindu Vishmith <ohkvmwelagedara@students.nsbm.ac.lk>
2025-03-22 22:12:05 +0530

    create footer

--- src/components/footer/footer.jsx
+++ src/components/footer/footer.jsx
@@ -1,53 +1,53 @@
-import { FiUpload } from "react-icons/fi";
-import { LuDownload } from "react-icons/lu";
-
 function Footer() {
     return (
-        <div className="flex flex-col w-full h-[900px] items-center relative">
-            {/* Footer image */}
-            <div className="h-[550px] w-5/6 absolute justify-center flex flex-row top-0 left-1/2 transform -translate-x-1/2 rounded-4xl shadow-2xl items-center px-16 z-20 gap-20" style={{ backgroundImage: "url('/src/assets/images/footerImg.png')" }}>
-                <div className="w-4/6 h-full rounded-4xl flex flex-col px-4 py-5 items-center justify-center">
-                    <div className="flex flex-col w-full pr-5">
-                        <div className="px-4 py-3 rounded-full border-2 border-[#035371] w-[250px] text-lg text-center text-[#035371] font-semibold">Start building today</div>
-                        <div className="text-6xl font-black mt-4"><span className="text-[#0C94C7]">Get your Matched</span> Jobs in a few minutes.</div>
-                        <div className="text-lg pt-4 pr-24">Advertise your jobs to millions of monthly users and search 15.8 Million CV in our database.</div>
-                        <div className="flex flex-row bg-white py-2 h-[65px] mt-4 rounded-full px-2 w-3/4 justify-between">
-                            <input
-                                type="text"
-                                placeholder="enter your email"
-                                className="rounded-md px-2 py-1 flex-1 border-0"
-                            />
-                            <button className="px-8 text-white h-full bg-[#035371] hover:bg-[#0C94C7] cursor-pointer rounded-full">Subscribe</button>
+        <div className="flex flex-col w-full h-[1100px] items-center">
+            {/* Footer GIF */}
+            <div className="h-[750px] w-full justify-center flex flex-row">
+                <img src="/src/assets/images/ContactUs.gif" alt="Footer Animation" className="w-full h-full object-cover rounded-4xl" />
+            </div>
+            <div className="flex flex-col bg-black w-full h-full px-20 py-20 justify-between">
+                <div className="flex flex-row w-full justify-between">
+                    <div className="flex flex-col">
+                        <div className="flex flex-row">
+                            <img src="/src/assets/icons/Rotaframe.svg" />
+                        </div>
+                        <div className="flex flex-row gap-4 mt-10">
+                            <button className="cursor-pointer"><img src="/src/assets/images/social1.svg" className="w-[20px] h-[20px]" /></button>
+                            <button className="cursor-pointer"><img src="/src/assets/images/social2.svg" className="w-[20px] h-[20px]" /></button>
+                            <button className="cursor-pointer"><img src="/src/assets/images/social3.svg" className="w-[20px] h-[20px]" /></button>
+                            <button className="cursor-pointer"><img src="/src/assets/images/social4.svg" className="w-[20px] h-[20px]" /></button>
+                        </div>
+                    </div>
+
+                    <div className="flex flex-row mt-4 gap-16">
+                        <div className="flex flex-col gap-2">
+                            <div className="text-lg text-[#FAFAFA] font-semibold mb-2">Services</div>
+                            <div className="text-normal text-[#898989] font-base">e-Commerce development</div>
+                            <div className="text-normal text-[#898989] font-base">Custom software development</div>
+                            <div className="text-normal text-[#898989] font-base">Mobile Application development</div>
+                            <div className="text-normal text-[#898989] font-base">UX/UI & Product development</div>
                         </div>
 
+                        <div className="flex flex-col gap-2">
+                            <div className="text-lg text-[#FAFAFA] font-semibold mb-2">Company</div>
+                            <div className="text-normal text-[#898989] font-base">Home</div>
+                            <div className="text-normal text-[#898989] font-base">Services</div>
+                            <div className="text-normal text-[#898989] font-base">Portfolio</div>
+                            <div className="text-normal text-[#898989] font-base">Contact</div>
+                        </div>
+
+                        <div className="flex flex-col gap-2">
+                            <div className="text-lg text-[#000000] font-semibold mb-2">.</div>
+                            <div className="text-normal text-[#898989] font-base">Terms of Service</div>
+                            <div className="text-normal text-[#898989] font-base">Privacy Policy</div>
+                        </div>
                     </div>
-
                 </div>
-                <div className="w-2/6 h-4/6 bg-white rounded-4xl flex flex-col px-4 py-2 items-center justify-center">
-                    <div className="flex flex-col gap-8">
-                        <button className="px-10 py-4 rounded-full flex flex-row gap-2 items-center font-semibold bg-[#ACE9FF] text-[#035371] border-2 border-[#035371]">
-                            <div className="w-4 h-4"><FiUpload /></div>
-                            <div>Upload your CV</div>
-                        </button>
-
-                        <button className="px-10 py-4 rounded-full flex flex-row gap-2 justify-center items-center font-semibold bg-[#ACE9FF] text-[#035371] border-2 border-[#035371]">
-                            <div className="w-4 h-4"><FiUpload /></div>
-                            <div>Create CV</div>
-                        </button>
-
-                        <button className="px-10 py-4 rounded-full flex flex-row gap-2 justify-center items-center font-semibold bg-[#ACE9FF] text-[#035371] border-2 border-[#035371]">
-                            <div className="w-4 h-4"><LuDownload /></div>
-                            <div>Download App</div>
-                        </button>
-                    </div>
-
-                </div>
-            </div>
-            <div className="h-[600px] w-full bg-[#042938] absolute justify-center bottom-0 left-1/2 transform -translate-x-1/2 shadow-2xl items-center px-8 z-2 flex flex-col">
-
+                <div className="flex flex-row w-full h-[1px] bg-[#2E2E2E] mt-12"></div>
+                <div className="flex flex-row w-full text-normal text-[#898989] justify-center mt-5">2025 © Rotaframe Technology</div>
             </div>
         </div>
-    )
+    );
 }
 
-export default Footer
+export default Footer;
